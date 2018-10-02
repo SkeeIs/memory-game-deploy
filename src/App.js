62bcaaf309dd43c9786fdde35cd41e0edafefde9
Taylor Skeels <skeels@Taylors-MacBook-Pro.local>
2018-10-01 19:13:47 -0700

    full file structure. some app.js, some navbar, gifs.json complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import gifs from "./gifs.json";
+import Card from "./components/Card";
+import Navbar from "./components/Nabar";
+import Modal from "./components/Modal";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+      
+      const j = Math.floor(Math.random() * (i + 1));
+      
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
 
 class App extends Component {
+  state = {
+    roundScore: 0,
+    highScore: 0,
+  }
   render() {
     return (
       <div className="App">
