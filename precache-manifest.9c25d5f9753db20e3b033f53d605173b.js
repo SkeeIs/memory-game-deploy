5831a7b2261ef71613ab7ba2c060c3c7c8f6fd8d
Taylor Skeels <skeels@Taylors-MacBook-Pro.local>
2018-10-03 19:55:06 -0700

    Updates

---
+++ precache-manifest.9c25d5f9753db20e3b033f53d605173b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7794b3764d0f54c55b01",
+    "url": "/memory-game-deploy/static/js/runtime~main.7794b376.js"
+  },
+  {
+    "revision": "b641b818e38cdb3c3889",
+    "url": "/memory-game-deploy/static/js/main.b641b818.chunk.js"
+  },
+  {
+    "revision": "7acc70d70cc4a4c11c80",
+    "url": "/memory-game-deploy/static/js/1.7acc70d7.chunk.js"
+  },
+  {
+    "revision": "b641b818e38cdb3c3889",
+    "url": "/memory-game-deploy/static/css/main.df477a29.chunk.css"
+  },
+  {
+    "revision": "7c976cde4f6b5e76145ad475f330d6d4",
+    "url": "/memory-game-deploy/index.html"
+  }
+];
